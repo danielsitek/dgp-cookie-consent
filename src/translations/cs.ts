ce75c63e308287e82b90833cafcfb9e459f3cd4f
Daniel Sitek <daniel.sitek@zoot.cz>
2022-01-04 12:16:02 +0100

    removed css aspect-ratio, updated copy, updated example

--- src/translations/cs.ts
+++ src/translations/cs.ts
@@ -5,37 +5,36 @@ const cs: CookieConsentTranslations = {
   tabAgree: {
     title: 'Souhlas',
     body: `
-      <p><strong>Sbíráme sušenky, abychom tě našli</strong></p>
-
-      <p>Chceš se na internetu dívat na reklamy, které tě pobaví a neštvou? Nebudeme ti nabízet hrnce ani krém na revma. Díky infomracím "cookies", na které nám dáš palec nahoru, ti můžeme naservírovat obsah na míru a vylepšovat naše služby.</p>
+     <p><strong>Tato webová stránka používá cookies</strong></p>
+     <p>K personalizaci obsahu a reklam, poskytování funkcí sociálních médií a analýze naší návštěvnosti využíváme soubory cookie. Informace o tom, jak náš web používáte, sdílíme se svými partnery pro sociální média, inzerci a analýzy. Partneři tyto údaje mohou zkombinovat s dalšími informacemi, které jste jim poskytli nebo které získali v důsledku toho, že používáte jejich služby.</p>
     `,
   },
   tabAbout: {
     title: 'O aplikaci',
     body: `
-      <p>Cookies jsou malé textové soubory, které mobou být používány webovými stránkami, aby učinily uživatelský zážitek více efektivní.</p>
-      <p>Zákon uvádí, že můžeme uklákadt cookies na vašem zařízení, pokud jsou nezbytně nutné pro provoz této stránky. Pro všechny ostatní typy cookies potřebujeme vaše povolení.</p>
+      <p>Cookies jsou malé textové soubory, které mohou být používány webovými stránkami, aby učinily uživatelský zážitek více efektivní.</p>
+      <p>Zákon uvádí, že můžeme ukládat cookies na vašem zařízení, pokud jsou nezbytně nutné pro provoz této stránky. Pro všechny ostatní typy cookies potřebujeme vaše povolení.</p>
       <p>Tato stránka používá různé typy cookies. Některé cookies jsou umístěny službami třetích stran, které se objevují na našich stránkách.</p>
-      <p>Kdykoliv můžete změnit nebo zrušit svůj souhlas prostřednictvím vyjádření o souborech cookies na našich webových stránkách.</p>
+      <p>Kdykoliv můžete změnit nebo zrušit svůj souhlas prostřednictvím Vyjádření o souborech cookie na našich webových stránkách.</p>
     `,
   },
   tabDetail: {
     title: 'Detail',
     necessary: {
       title: 'Nutné',
-      perex: 'Nutné cookies pomáhají, aby byla webová stránka použitelná tak, že umožní základní funkce jako navigace stránky a přístup k zabezpečeným sekcím webové stránky.',
+      perex: 'Nutné cookies pomáhají, aby byla webová stránka použitelná tak, že umožní základní funkce jako navigace stránky a přístup k zabezpečeným sekcím webové stránky. Webová stránka nemůže správně fungovat bez těchto cookies.',
     },
     preferences: {
-      title: 'Preferences',
-      perex: 'Preference cookies enable a website to remember information that changes the way the website behaves or looks, like your preferred language or the region that you are in.',
+      title: 'Preferenční',
+      perex: 'Preferenční cookies umožňují, aby si webová stránka zapamatovala informace, které mění, jak se webová stránka chová nebo jak vypadá. Je to například preferovaný jazyk nebo region, kde se nacházíte.',
     },
     statistics: {
       title: 'Statistické',
-      perex: 'Statistic cookies help website owners to understand how visitors interact with websites by collecting and reporting information anonymously.',
+      perex: 'Statistické cookies pomáhají majitelům webových stránek, aby porozuměli, jak návštěvníci používají webové stránky. Anonymně sbírají a sdělují informace.',
     },
     marketing: {
       title: 'Marketingové',
-      perex: 'Marketing cookies are used to track visitors across websites. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.',
+      perex: 'Marketingové cookies jsou používány pro sledování návštěvníků na webových stránkách. Záměrem je zobrazit reklamu, která je relevantní a zajímavá pro jednotlivého uživatele a tímto hodnotnější pro vydavatele a inzerenty třetích stran.',
     },
   },
   buttonEdit: {
@@ -48,7 +47,7 @@ const cs: CookieConsentTranslations = {
     label: 'Odmítnout vše',
   },
   buttonConfirm: {
-    label: 'Potvrdit',
+    label: 'Uložit',
   },
   lastUpdated: 'Prohlášení o cookies bylo naposledy aktualizováno %date.',
 };
