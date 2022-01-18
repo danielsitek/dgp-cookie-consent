35a31236d78b125a2cba2b617a376c41935c52c3
Daniel Sitek <daniel.sitek@zoot.cz>
2022-01-18 08:19:14 +0100

    release v1.3.0

---
+++ src/themes/fitrazdva.ts
@@ -0,0 +1,13 @@
+import { CookieConsentTheme } from '../services/theme-service';
+
+export const themeFitrazdva: CookieConsentTheme = {
+  'color-primary': '#781036',
+
+  'button-primary__bg-color': '#781036',
+  'button-primary__color': '#fff',
+  'button-primary__text-transform': 'none',
+
+  'button-primary--hover__bg-color': '#8d1a44',
+  'button-primary--hover__color': '#fff',
+  'button-primary--hover__text-transform': 'none',
+};
