44b2aa5ef9d9133a9417f01b552e80d353ba3912
Daniel Sitek <daniel.sitek@zoot.cz>
2022-01-31 11:55:41 +0100

    left only changed theme props

--- src/themes/different.ts
+++ src/themes/different.ts
@@ -1,37 +1,18 @@
 import { CookieConsentTheme } from '../services/theme-service';
 
 export const themeDifferent: CookieConsentTheme = {
-  'base-color': '#393939',
-  'base-font-size': '15px',
-  'base-line': '1px solid #d0d0d0',
-  'base-font-family': 'sans-serif',
-  'border-radius': '6px',
-
-  'color-grey': '#d6d6d6',
   'color-primary': '#008c95',
-  'color-text-light': '#757575',
-  'color-text': '#242424',
-  'color-white': '#fff',
 
   'button-default__bg-color': '#fff',
   'button-default__color': '#676767',
-  'button-default__text-transform': 'none',
   'button-default__border': '2px solid #676767',
-  'button-default__box-shadow': 'none',
 
-  'button-default--hover__bg-color': '#fff',
   'button-default--hover__color': '#676767',
   'button-default--hover__border': '2px solid #676767',
-  'button-default--hover__box-shadow': '0 0 17px 0 rgba(0,0,0,.1)',
 
   'button-primary__bg-color': '#008c95',
   'button-primary__color': '#fff',
-  'button-primary__text-transform': 'uppercase',
-  'button-primary__border': '0 none',
-  'button-primary__box-shadow': 'none',
 
   'button-primary--hover__bg-color': '#007379',
   'button-primary--hover__color': '#fff',
-  'button-primary--hover__border': '0 none',
-  'button-primary--hover__box-shadow': 'none',
 };
--- src/themes/zoot.ts
+++ src/themes/zoot.ts
@@ -1,37 +1,5 @@
 import { CookieConsentTheme } from '../services/theme-service';
 
 export const themeZoot: CookieConsentTheme = {
-  'base-color': '#393939',
-  'base-font-size': '15px',
-  'base-line': '1px solid #d0d0d0',
-  'base-font-family': 'sans-serif',
-  'border-radius': '6px',
-
-  'color-grey': '#d6d6d6',
-  'color-primary': '#f8c132',
-  'color-text-light': '#757575',
-  'color-text': '#242424',
-  'color-white': '#fff',
-
-  'button-default__bg-color': '#f4f4f4',
-  'button-default__color': '#242424',
-  'button-default__text-transform': 'none',
-  'button-default__border': '1px solid #f4f4f4',
-  'button-default__box-shadow': 'none',
-
-  'button-default--hover__bg-color': '#fff',
-  'button-default--hover__color': '#242424',
-  'button-default--hover__border': '1px solid #d1d1d1',
-  'button-default--hover__box-shadow': '0 0 17px 0 rgba(0,0,0,.1)',
-
-  'button-primary__bg-color': '#f8c132',
-  'button-primary__color': '#242424',
-  'button-primary__text-transform': 'uppercase',
-  'button-primary__border': '0 none',
-  'button-primary__box-shadow': 'none',
-
-  'button-primary--hover__bg-color': '#efaf08',
-  'button-primary--hover__color': '#242424',
-  'button-primary--hover__border': '0 none',
-  'button-primary--hover__box-shadow': 'none',
+  /* ZOOT is default default theme */
 };
