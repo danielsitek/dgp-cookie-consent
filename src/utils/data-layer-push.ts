09854beae4612f795104274a5814ad1291279454
Daniel Sitek <dan.sitek@gmail.com>
2023-04-11 17:09:15 +0200

    fixed codestyle issues

--- src/utils/data-layer-push.ts
+++ src/utils/data-layer-push.ts
@@ -1,4 +1,4 @@
-export function dataLayerPush(...args: unknown[]) {
+export function dataLayerPush(...args: unknown[]): void {
   window.dataLayer = window.dataLayer || [];
 
   if (args.length === 1) {
