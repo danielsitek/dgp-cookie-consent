d97b4e75bd88cf9ba4dbac35b7be872c02974641
Daniel Sitek <dan.sitek@gmail.com>
2023-11-27 17:44:33 +0100

    created new dispatch events

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,10 +1,23 @@
-import { EVENT_CONSENT_HIDE, EVENT_CONSENT_READY, EVENT_CONSENT_SHOW, EVENT_CONSENT_UPDATED, EVENT_DEBOUNCE, EVENT_DEBOUNCE_FAST } from '../config';
+import {
+  EVENT_BADGE_CLICK,
+  EVENT_BADGE_SHOW,
+  EVENT_CONSENT_CLOSE,
+  EVENT_CONSENT_HIDE,
+  EVENT_CONSENT_READY,
+  EVENT_CONSENT_SHOW,
+  EVENT_CONSENT_UPDATED,
+  EVENT_DEBOUNCE,
+  EVENT_DEBOUNCE_FAST,
+} from '../config';
 import { debounce } from './debounce';
 
 export const eventConsentUpdated = new Event(EVENT_CONSENT_UPDATED);
 export const eventConsentShow = new Event(EVENT_CONSENT_SHOW);
 export const eventConsentHide = new Event(EVENT_CONSENT_HIDE);
 export const eventConsentReady = new Event(EVENT_CONSENT_READY);
+export const eventConsentClose = new Event(EVENT_CONSENT_CLOSE);
+export const eventBadgeShow = new Event(EVENT_BADGE_SHOW);
+export const eventBadgeClick = new Event(EVENT_BADGE_CLICK);
 
 export const dispatchEventConsentUpdated = debounce(() => {
   window.dispatchEvent(eventConsentUpdated);
@@ -20,4 +33,16 @@ export const dispatchEventConsentHide = debounce(() => {
 
 export const dispatchEventConsentReady = debounce(() => {
   window.dispatchEvent(eventConsentReady);
+}, EVENT_DEBOUNCE_FAST);
+
+export const dispatchEventConsentClose = debounce(() => {
+  window.dispatchEvent(eventConsentClose);
+}, EVENT_DEBOUNCE_FAST);
+
+export const dispatchEventBadgeShow = debounce(() => {
+  window.dispatchEvent(eventBadgeShow);
+}, EVENT_DEBOUNCE_FAST);
+
+export const dispatchEventBadgeClick = debounce(() => {
+  window.dispatchEvent(eventBadgeClick);
 }, EVENT_DEBOUNCE_FAST);
