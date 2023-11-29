0741f207e9876807077bfcd807dd2259585c489c
Daniel Sitek <dan.sitek@gmail.com>
2023-11-29 14:05:08 +0100

    refactored events settings

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -12,43 +12,36 @@ import {
 } from '../config';
 import { debounce } from './debounce';
 
-export const eventConsentUpdated = new Event(EVENT_CONSENT_UPDATED);
-export const eventConsentShow = new Event(EVENT_CONSENT_SHOW);
-export const eventConsentHide = new Event(EVENT_CONSENT_HIDE);
-export const eventConsentReady = new Event(EVENT_CONSENT_READY);
-export const eventConsentClose = new Event(EVENT_CONSENT_CLOSE);
-export const eventBadgeShow = new Event(EVENT_BADGE_SHOW);
-export const eventBadgeClick = new Event(EVENT_BADGE_CLICK);
-export const eventBadgeHide = new Event(EVENT_BADGE_HIDE);
+const dispatchEvent = (eventName: string) => window.dispatchEvent(new Event(eventName));
 
 export const dispatchEventConsentUpdated = debounce(() => {
-  window.dispatchEvent(eventConsentUpdated);
+  dispatchEvent(EVENT_CONSENT_UPDATED);
 }, EVENT_DEBOUNCE);
 
 export const dispatchEventConsentShow = debounce(() => {
-  window.dispatchEvent(eventConsentShow);
+  dispatchEvent(EVENT_CONSENT_SHOW);
 }, EVENT_DEBOUNCE_FAST);
 
 export const dispatchEventConsentHide = debounce(() => {
-  window.dispatchEvent(eventConsentHide);
+  dispatchEvent(EVENT_CONSENT_HIDE);
 }, EVENT_DEBOUNCE_FAST);
 
 export const dispatchEventConsentReady = debounce(() => {
-  window.dispatchEvent(eventConsentReady);
+  dispatchEvent(EVENT_CONSENT_READY);
 }, EVENT_DEBOUNCE_FAST);
 
 export const dispatchEventConsentClose = debounce(() => {
-  window.dispatchEvent(eventConsentClose);
+  dispatchEvent(EVENT_CONSENT_CLOSE);
 }, EVENT_DEBOUNCE_FAST);
 
 export const dispatchEventBadgeShow = debounce(() => {
-  window.dispatchEvent(eventBadgeShow);
+  dispatchEvent(EVENT_BADGE_SHOW);
 }, EVENT_DEBOUNCE_FAST);
 
 export const dispatchEventBadgeClick = debounce(() => {
-  window.dispatchEvent(eventBadgeClick);
+  dispatchEvent(EVENT_BADGE_CLICK);
 }, EVENT_DEBOUNCE_FAST);
 
 export const dispatchEventBadgeHide = debounce(() => {
-  window.dispatchEvent(eventBadgeHide);
+  dispatchEvent(EVENT_BADGE_HIDE);
 }, EVENT_DEBOUNCE_FAST);
