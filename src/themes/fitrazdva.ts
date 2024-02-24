454dd70827621c213219f224cbcabb7c109ec9a0
Daniel Sitek <dan.sitek@gmail.com>
2024-02-24 12:02:57 +0100

    removed unknown theme property

--- src/themes/fitrazdva.ts
+++ src/themes/fitrazdva.ts
@@ -9,5 +9,4 @@ export const themeFitrazdva: CookieConsentTheme = {
 
   'button-primary--hover__bg-color': '#8d1a44',
   'button-primary--hover__color': '#fff',
-  'button-primary--hover__text-transform': 'none',
 };
