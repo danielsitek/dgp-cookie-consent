b2fd600dfe9c43afba5a47fbd10acab240584fb2
Daniel Sitek <dan.sitek@gmail.com>
2024-02-24 13:57:06 +0100

    merge same rollupJS piping

--- tasks/rollup-gtm.js
+++ tasks/rollup-gtm.js
@@ -1,15 +1,9 @@
-const { src, dest } = require('gulp');
-const rollupJs = require('rollup-stream-gulp');
 const typescript = require('@rollup/plugin-typescript');
 const { nodeResolve: resolve } = require('@rollup/plugin-node-resolve');
 const { isDevelopment } = require('./helpers/environment');
-const rename = require('gulp-rename');
-const terser = require('gulp-terser');
-const stripDebug = require('gulp-strip-debug');
-const strip = require('gulp-strip-comments');
-const insert = require('gulp-insert');
 
 const packageJson = require('../package.json');
+const rollupPipe = require('./helpers/rollupPipe');
 
 const createRollupConfig = () => ({
   input: 'src/gtm.ts',
@@ -21,28 +15,12 @@ const createRollupConfig = () => ({
   plugins: [resolve(), typescript()],
 });
 
-module.exports = async function rollup() {
+module.exports = async function rollupGtm() {
   const rollupConfig = createRollupConfig();
 
-  return src(['gtm.ts'], {
-    cwd: 'src',
-  })
-    .pipe(rollupJs(rollupConfig))
-    .pipe(strip())
-    .pipe(dest('./dist'))
-    .pipe(stripDebug())
-    .pipe(
-      terser({
-        mangle: {
-          reserved: ['ConsentService'],
-        },
-      }),
-    )
-    .pipe(insert.prepend(`/* v${packageJson.version} */`))
-    .pipe(
-      rename({
-        suffix: '.min',
-      }),
-    )
-    .pipe(dest('./dist'));
+  return rollupPipe({
+    file: 'gtm.ts',
+    rollupConfig,
+    packageJson,
+  });
 };
